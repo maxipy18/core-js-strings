1f8f8ab49e514e3e71058a50bae3176e422ad58b
Maxim Krestyaninov <maxipy@ya.ru>
2024-11-10 18:44:16 +0400

    feat: add sumOfCodes function

--- src/strings-tasks.js
+++ src/strings-tasks.js
@@ -187,8 +187,13 @@ function removeLastOccurrences(str, value) {
  *   sumOfCodes('') => 0
  *   sumOfCodes() => 0
  */
-function sumOfCodes(/* str */) {
-  throw new Error('Not implemented');
+function sumOfCodes(str) {
+  if (typeof str !== 'string') return 0;
+  let sum = 0;
+  for (let i = 0; i < str.length; i += 1) {
+    sum += str.charCodeAt(i);
+  }
+  return sum;
 }
 
 /**
